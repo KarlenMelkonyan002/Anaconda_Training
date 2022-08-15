87109e860431ecce543f093b298f4d6e60fbee5e
KarlenMelkonyan007 <72556888+KarlenMelkonyan007@users.noreply.github.com>
2022-08-15 22:01:09 +0400

    Delete Day 1 - Aug 5 directory

--- Day 1 - Aug 5/range_evens.py
+++
@@ -1,9 +0,0 @@
-def range_evens(start, end):
-    res = []
-    for num in range(start, end + 1):
-        if num % 2 != 0:
-            res.append(num)
-    return tuple(res)
-
-
-print(range_evens(3, 7))
